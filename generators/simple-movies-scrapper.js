7c3c2bd3cf9dbb26208cc0a78c7875543dc36e7c
Peter Velkov <kidroca@gmail.com>
2016-11-21 22:13:12 +0200

    Updated simple-movies-scrapper
    Changes in order to use the generator in parallel

--- generators/simple-movies-scrapper.js
+++ generators/simple-movies-scrapper.js
@@ -4,20 +4,20 @@ const constants = require('../config/constants');
 const urlGenerator = require('./url-generator');
 const moviesGenerator = require('./movies-generator');
 
+urlGenerator.config({
+    genres: constants.genres,
+    pagesCount: constants.pagesCount
+});
+
+let urlQueue = urlGenerator.createUrlsQueue();
+
+moviesGenerator.config({ urlQueue });
+
 module.exports = {
     generate
 };
 
 function generate() {
-
-    urlGenerator.config({
-        genres: constants.genres,
-        pagesCount: constants.pagesCount
-    });
-
-    let urlQueue = urlGenerator.createUrlsQueue();
-
-    moviesGenerator.config({ urlQueue });
 
     return moviesGenerator.generateMovies();
 }
